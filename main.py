ed8d2f8e6f8f8927843af07357096e10798059de
Adri-2424 <adriannajordyn.grant@yahoo.com>
2024-07-23 21:42:42 -0400

    updated

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ def encode(password):
 
     for i in range(len(password)):
         encoded_password += str((int(password[i]) + 3))
+    return encoded_password
+
+def decode(password):
+    decoded_password = []
+    for char in password:
+        newChar = (int(char) - 3)%10
+        decoded_password.append(str(newChar))
+    return "".join(decoded_password)
 
 
 def main():
@@ -21,6 +29,7 @@ def main():
             decoded_password = input("Please enter the password to encode: ")
             encoded_passord = encode(decoded_password)
             print("Your password has been encoded and stored!")
+            print(encoded_passord)
         elif option == 2:
             continue
         elif option == 3:
