8359d98fdf6d4585e5e8b3b9ae0f0d0ac648c250
amandacmwood <127638111+amanda-cw@users.noreply.github.com>
2024-07-25 10:22:25 -0400

    Completed menu function

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ def encode(password):
         encoded_password += str((int(password[i]) + 3))
     return encoded_password
 
+
 def decode(password):
     decoded_password = []
     for char in password:
-        newChar = (int(char) - 3)%10
+        newChar = (int(char) - 3) % 10
         decoded_password.append(str(newChar))
     return "".join(decoded_password)
 
@@ -30,7 +31,10 @@ def main():
             encoded_password = encode(decoded_password)
             print("Your password has been encoded and stored!")
         elif option == 2:
-            continue
+            decoded_password = decode(encoded_password)
+            print(
+                f"The encoded password is {encoded_password}, and the original password is {decoded_password}."
+            )
         elif option == 3:
             quit()
         else:
